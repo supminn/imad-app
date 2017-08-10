19152da615db5bd5a4bcf1e0207fb22cd5b36d0b
supminn <30731236+supminn@users.noreply.github.com>
2017-08-10 21:31:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title: 'Supriya\'s Profile',
+    heading:'profile details',
+    
+    content:`
+         <p>
+            NAME: Supriya Minnasandram<br/>
+            Education<br/>
+            Work Experience<br/>
+            Hobbies & Interests<br/>
+            DOB
+        </p>
+        
+        <h4> About Myself</h4>
+            <p>
+                I am trying to explore new things and trying to improve in coding with the help of IMAD :)
+            </p>`
+};
+
+function createtemp (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var htmltemp = `
+    
+    <html>
+        <head>
+            <title>
+          ${title} 
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <link href="/ui/style.css" rel="stylesheet" />
+          
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                <a href="/">HOME</a>
+                </div>
+                <hr/>
+                <hr/>
+                <div>
+                    <h2>
+                        ${heading}
+                    </h2>
+                </div>
+                <div>
+                  ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmltemp;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/profile',function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+    res.send(createtemp(profile));
 });
 
 app.get('/articles',function(req,res) {
