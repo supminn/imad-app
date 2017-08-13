2e775d1432fb8572a0adbd2f26eea86b5d0c77a1
supminn <30731236+supminn@users.noreply.github.com>
2017-08-13 21:05:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter+1;
+    res.send(counter.toString());
+});
+
 app.get('/profile',function(req,res) {
     res.send(createtemp(profile));
 });
