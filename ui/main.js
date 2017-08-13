3314d970c5a93b88e7f03ede426d35693139f514
supminn <30731236+supminn@users.noreply.github.com>
2017-08-13 13:05:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 
-var element = document.getElementbyId('main text');
+var element = document.getElementById('main text');
 element.innerHTML='Supriya\'s webapp coming soon...';
