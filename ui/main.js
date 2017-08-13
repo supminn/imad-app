464f0ab8da487ddd80b362ccde1e5c9eb842b3cc
supminn <30731236+supminn@users.noreply.github.com>
2017-08-13 19:42:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('main text');
 element.innerHTML='Supriya\'s webapp coming soon...';
 //move the img
-var img = document.getElementById('img');
+var img = document.getElementById('supi');
 img.onlick = function(){
    img.style.marginleft =  '100px';
 };
