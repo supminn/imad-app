ac5fb320da75c9a45d810305deb62d3e65ac476a
supminn <30731236+supminn@users.noreply.github.com>
2017-08-13 12:52:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,8 +76,8 @@ app.get('/videos',function(req,res) {
     res.send('Supriya\'s performance videos will be served here');
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
